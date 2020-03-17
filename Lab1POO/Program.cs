df9206f6a32f44f3ed4be548fd0c9e9d1f532426
mrojasg <usuario@MacBook-Air-de-Usuario.local>
2020-03-17 15:31:35 -0300

    first commit

---
+++ Lab1POO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1POO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
